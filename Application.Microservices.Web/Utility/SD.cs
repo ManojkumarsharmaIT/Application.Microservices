f74df2d33911dfa9aeb21b112f9db7cb7fd99d2c
Manoj Kumar Sharma <Manoj Kumar Sharma@LAPTOP-6FV2EOAR>
2025-09-16 22:15:15 +0530

    Added Gateway Solution

--- Application.Microservices.Web/Utility/SD.cs
+++ Application.Microservices.Web/Utility/SD.cs
@@ -9,8 +9,10 @@
 
 
         public static string AuthAPIBase { get; set; }
-        public const string RoleAdmin = "Admin";
-        public const string RoleCustomer = "Customer";
+       
+      
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
         public const string TokenCookie = "JWTToken";
         public enum ApiType
         {
@@ -20,13 +22,14 @@
             DELETE
         }
 
-
         public const string Status_Pending = "Pending";
         public const string Status_Approved = "Approved";
         public const string Status_ReadyForPickup = "ReadyForPickup";
         public const string Status_Completed = "Completed";
         public const string Status_Refunded = "Refunded";
         public const string Status_Cancelled = "Cancelled";
+
+      
 
         public enum ContentType
         {
